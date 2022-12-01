2eefa58ae8d87fbf8597e9871c919aca82c9dee5
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2022-11-30 21:37:20 -0300

    criando funcoes de conexao com o banco

--- account/src/repositories/accountRepository.js
+++ account/src/repositories/accountRepository.js
@@ -1,3 +1,28 @@
-export function saveAccount (conta) {
-    
+import { MongoClient } from 'mongodb';
+
+async function dataBaseConnect() {
+    const connectionURL = 'mongodb://admin:admin@account-database:27017'
+    const connection = new MongoClient(connectionURL);
+    await connection.connect();
+    const database = connection.db('accounts')
+
+    return database.collection('users');
+}
+
+export async function saveAccount(account) {
+    const collection = await dataBaseConnect();
+    await collection.insertOne(account);
+}
+
+export async function findAccountByEmail(email) {
+    const collection = await dataBaseConnect();
+    const account = await collection.findOne({ email });
+
+    return account;
+}
+
+export async function existsAccountByEmail(email) {
+    const account = await findAccountByEmail(email);
+
+    return account !== null;
 }
