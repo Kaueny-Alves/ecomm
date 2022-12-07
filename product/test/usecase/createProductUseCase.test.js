2855db50170f710935f7c9cc38a27230a1044c5d
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2022-12-07 13:24:19 -0300

    criando arquivo de produtos

--- product/test/usecase/createProductUseCase.test.js
+++ product/test/usecase/createProductUseCase.test.js
@@ -1,25 +1,5 @@
 import { createProductUseCase } from "../../src/usecase/createProductUseCase.js";
-
-const produto =
-{
-    nome: "coca",
-    valor: 2,
-    quantidade: 1,
-    descricao: "refrigerante lata",
-    categoria: "bebidas",
-    caracteristicas: [
-        {
-            nome: "refrigerante",
-            descricao: " bebidas",
-        }
-    ],
-    imagens: [
-        {
-            url: "https://pixabay.com/pt/photos/posso-lata-de-cola-cola-bebida-592366/",
-            descricao: "refrigerante de lata",
-        }
-    ],
-}
+import { produto } from "./products.js";
 
 
 const product = await createProductUseCase(produto);
--- product/test/usecase/listProducts.test.js
+++ product/test/usecase/listProducts.test.js
@@ -1,5 +1,6 @@
 import { listProducts } from "../../src/usecase/listProducts.js"
 import { createProductUseCase } from "../../src/usecase/createProductUseCase.js";
+import { produto } from "./products.js";
 
 
 
@@ -7,70 +8,6 @@ const productsList = await listProducts();
 console.log("array vazio: ", productsList);
 
 
-const coca =
-{
-    nome: "coca",
-    valor: 2,
-    quantidade: 1,
-    descricao: "refrigerante lata",
-    categoria: "bebidas",
-    caracteristicas: [
-        {
-            nome: "refrigerante",
-            descricao: " bebidas",
-        }
-    ],
-    imagens: [
-        {
-            url: "https://pixabay.com/pt/photos/posso-lata-de-cola-cola-bebida-592366/",
-            descricao: "refrigerante de lata",
-        }
-    ],
-}
-const coca1 =
-{
-    nome: "coca",
-    valor: 2,
-    quantidade: 1,
-    descricao: "refrigerante lata",
-    categoria: "bebidas",
-    caracteristicas: [
-        {
-            nome: "refrigerante",
-            descricao: " bebidas",
-        }
-    ],
-    imagens: [
-        {
-            url: "https://pixabay.com/pt/photos/posso-lata-de-cola-cola-bebida-592366/",
-            descricao: "refrigerante de lata",
-        }
-    ],
-}
-const coca2 =
-{
-    nome: "coca",
-    valor: 2,
-    quantidade: 1,
-    descricao: "refrigerante lata",
-    categoria: "bebidas",
-    caracteristicas: [
-        {
-            nome: "refrigerante",
-            descricao: " bebidas",
-        }
-    ],
-    imagens: [
-        {
-            url: "https://pixabay.com/pt/photos/posso-lata-de-cola-cola-bebida-592366/",
-            descricao: "refrigerante de lata",
-        }
-    ],
-}
-
-await createProductUseCase(coca);
-await createProductUseCase(coca1);
-await createProductUseCase(coca2);
-
+await createProductUseCase(produto);
 
 console.log("lista de produtos: ", productsList);
