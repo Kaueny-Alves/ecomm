3b0951275cd289f06d14675b76cf66ac03ee8a4a
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2022-12-12 19:53:21 -0300

    add validação e try catch

--- account/src/routes.js
+++ account/src/routes.js
@@ -8,12 +8,29 @@ router.get('/', (req, res) => {
 });
 
 router.post('/accounts', async function (req, res) {
-    const nameUser = req.body.nameUser
-    const email = req.body.email
-    const password = req.body.password
-    const account = await createUserUseCase(nameUser, email, password);
 
-    res.status(201).json(account);
+
+    try {
+        const { nameUser, email, password } = req.body
+        const account = await createUserUseCase(nameUser, email, password);
+
+        if (!account) {
+            res.status(400).send('Something broke when creating account!');
+            return 
+        }
+
+        const user = {
+            name: account.name,
+            email: account.email,
+            date: account.createdDate
+        }
+
+        res.status(201).json(user);
+
+    } catch (e) {
+        console.error("Error when creating account: ", e.message.stack);
+    }
+
 });
 
 
--- account/test/createUserAccount.test.js
+++ account/test/createUserAccount.test.js
@@ -2,5 +2,5 @@
 import { createUserUseCase } from "../src/use-case/createUserAccount.js";
 
     console.log('Creating account ======= ')
-    const account = await createUserUseCase('ka', 'email@email.com', '123456');
+    const account = await createUserUseCase('kaueny', 'kaueny@email.com', '123456');
     console.log("account: ", account)
