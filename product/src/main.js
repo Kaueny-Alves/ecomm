8fa4bc28b3c69261e5e496afdd403e5063107dca
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2022-12-21 21:50:33 -0300

    documentando a api

--- product/src/main.js
+++ product/src/main.js
@@ -1,12 +1,15 @@
 
 import express, { json } from 'express';
 import { router } from './routes.js';
+import swaggerUi from "swagger-ui-express";
+import apiDocs from '../api-docs.json' assert { type: "json" };
 
 const port = 3000;
 const app = express();
 
 app.use(json());
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs))
 
 app.listen(port, () => {
   console.log(`Products app listening on port ${port}`)
