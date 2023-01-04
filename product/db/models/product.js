31d1bd6d81f81a31a5ecd8603a24fb49ea852dc8
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2023-01-04 20:23:44 -0300

    add config do mysql

---
+++ product/db/models/product.js
@@ -0,0 +1,25 @@
+'use strict';
+import { Model } from 'sequelize';
+
+
+
+
+export default (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
--- product/src/main.js
+++ product/src/main.js
@@ -4,14 +4,24 @@ import { router } from './routes.js';
 import cors from 'cors';
 import swaggerUi from "swagger-ui-express";
 import apiDocs from '../api-docs.json' assert { type: "json" };
+import {Sequelize} from 'sequelize';
+
 
 const port = 3000;
 const app = express();
+const sequelize = new Sequelize('mysql://admin:admin@localhost:3306/products')
 
 app.use(json());
 app.use(cors());
 app.use(router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs))
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.listen(port, () => {
   console.log(`Products app listening on port ${port}`)
