3abc72304aa4b478fbcb537d34b4d0234ea5d6e0
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2023-01-13 00:23:45 -0300

    ajustes dos modelos

--- product/src/main.js
+++ product/src/main.js
@@ -1,23 +1,32 @@
 
 import express, { json } from 'express';
-import body from 'body-parser'
 import { router } from './routes.js';
 import cors from 'cors';
 import swaggerUi from "swagger-ui-express";
-import apiDocs from '../api-docs.json' assert { type: "json" };
+import yaml from 'yamljs';
+import { client } from './repositories/clientDatabase.js';
 
 const port = 3000;
 const app = express();
+const swaggerDocs = yaml.load('api-docs.yaml');
 
-app.use(body.json());
+app.use(json());
 app.use(cors());
 app.use(router);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+const Connection = async () => {
+  try {
+    await client.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error.message);
+  }
+}
 
 app.listen(port, () => {
-  
   console.log(`Products app listening on port ${port}`)
-
+  Connection();
 });
 
 
