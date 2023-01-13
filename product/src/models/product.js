1757826b9e2815fb0ef4b83719fcd43a6131647d
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2023-01-13 11:58:56 -0300

    ajustes do modelo de produto

--- product/src/models/product.js
+++ product/src/models/product.js
@@ -1,28 +1,39 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Product extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Product.init({
-    name: DataTypes.STRING,
-    value: DataTypes.DECIMAL,
-    quantity: DataTypes.INTEGER,
-    description: DataTypes.TEXT,
-    category: DataTypes.STRING,
-    id_user: DataTypes.UUID
-  }, {
-    sequelize,
-    modelName: 'Product',
-  });
-  return Product;
-};
+
+import { Model, DataTypes } from 'sequelize';
+import { client } from '../repositories/clientDatabase.js';
+import { ProductFeature } from './product_feature.js';
+import { ProductImage } from './product_image.js';
+
+export class Product extends Model {
+  static associate(models) {}
+}
+Product.init({
+  name: DataTypes.STRING,
+  value: DataTypes.DECIMAL,
+  quantity: DataTypes.INTEGER,
+  description: DataTypes.TEXT,
+  category: DataTypes.STRING,
+  id_user: DataTypes.UUID
+}, {
+  sequelize: client,
+  modelName: 'Product',
+});
+
+Product.ProductImage = Product.hasMany(ProductImage, {
+  foreignKey: 'product_id',
+  as: 'images'
+});
+
+Product.ProductFeature = Product.hasMany(ProductFeature, {
+  foreignKey: 'product_id',
+  as: 'features'
+});
+
+ProductFeature.belongsTo(Product, {
+  foreignKey: 'id',
+});
+
+
+ProductImage.belongsTo(Product, {
+  foreignKey: 'id',
+});
