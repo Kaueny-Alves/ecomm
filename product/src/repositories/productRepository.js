767b48753ccf2078b8c06833bf6f5574908dab76
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2023-01-14 10:27:57 -0300

    adiciona lib jest

--- product/src/repositories/productRepository.js
+++ product/src/repositories/productRepository.js
@@ -2,30 +2,37 @@ import { Product } from '../models/product.js';
 import { ProductFeature } from '../models/product_feature.js';
 import { ProductImage } from '../models/product_image.js';
 
-
 export async function saveProduct(product) {
+    try {
+        const createdProduct = await Product.create(product, {
+            include: [
+                { association: Product.ProductFeature, as: 'features', },
+                { association: Product.ProductImage, as: 'images' }
+            ]
 
-    const createdProduct = await Product.create(product, {
-        include: [
-            { association: Product.ProductFeature, as: 'features', },
-            { association: Product.ProductImage, as: 'images' }
-        ]
+        });
+        await createdProduct.save();
+        return createdProduct;
 
-    });
-    await createdProduct.save();
-    return createdProduct;
+    } catch (e) {
+        console.error("error creating product =======", e.message.stack);
+    } 
 }
 
 export async function findProducts() {
-    const allProducts = await Product.findAll({
-        include: [
-            {
-                model: ProductFeature,
-                as: 'features'
-            }, {
-                model: ProductImage,
-                as: 'images'
-            }]
-    });
-    return allProducts
+    try {
+        const allProducts = await Product.findAll({
+            include: [
+                {
+                    model: ProductFeature,
+                    as: 'features'
+                }, {
+                    model: ProductImage,
+                    as: 'images'
+                }]
+        });
+        return allProducts
+    } catch (e) {
+        console.error("error fetching product =======", e.message.stack);
+    }
 }
