42f0f9d2326195a18d0a31e9624c8f0ee755be2e
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2023-01-23 16:37:03 -0300

    atualiza documentação

--- product/src/routes.js
+++ product/src/routes.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { createProductUseCase } from "../src/use-case/createProductUseCase.js";
 import { tokenValidated } from './middleware/authenticator.js';
-import * as jwt from "jsonwebtoken";
 import { listProducts } from './use-case/listProducts.js';
 
 export const router = Router();
@@ -12,7 +11,7 @@ router.get('/products', (req, res) => {
             res.status(200).json(data);
         })
         .catch((error) => {
-            res.status(400).json({ status: 'Error getting product!', message: error.message });
+            res.json({ status: 'Error getting product!', message: error.message });
         })
 });
 
@@ -25,12 +24,13 @@ router.post('/products', function (req, res) {
     const id = payload.id
 
     const product = req.body
+    
     createProductUseCase(product, id)
         .then((data) => {
             res.status(201).json(data);
         })
         .catch((error) => {
-            res.status(400).json({ status: 'Error fetching products!', message: error.message });
+            res.json({ status: 'Error fetching products!', message: error.message });
         })
 });
 
