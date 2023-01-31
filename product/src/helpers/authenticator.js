dc8014907d24a46268d85cc910d1c672dd1d98b6
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2023-01-31 18:59:24 -0300

    ajustes de código

--- product/src/helpers/authenticator.js
+++ product/src/helpers/authenticator.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export function tokenValidated(req, res) {
 
     try {
-        const token = req.headers.authorization;
+        const token = req.headers.authorization.split(' ')[1];
         if (!token) return res.status(401).send('Access denied. No token provided.')
 
         const payload = jwt.verify(token, process.env.JWT_KEY);
