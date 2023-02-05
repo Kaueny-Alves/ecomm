a6035cb0076671cfd4df709a7fd8f00a835c81b0
Kaueny de Oliveira Alves <kauenyalves@hotmail.com>
2023-02-04 21:37:43 -0300

    remove console

--- product/src/use-case/createProductUseCase.js
+++ product/src/use-case/createProductUseCase.js
@@ -38,7 +38,7 @@ export async function createProductUseCase(product, id) {
     }
 
     const products = await saveProduct(createProduct)
-    console.log(products)
+  
     return {
         hasErrors: false,
         errors: undefined,
